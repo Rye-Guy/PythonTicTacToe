e32994fb0f587e6a7a1e35536e6e25bbfd1aa38d
Rye-Guy <ryanracioppo95@gmail.com>
2018-11-06 17:22:51 -0500

    added main functions

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+print('Welcome to Tic Tac Toe!') 
+
 def display_board(board):
     print('------ ' + ' ----- ' + ' -----')
     print('------ ' + '|' + board[0] + '|' + board[1] + '|' + board[2] + '|' + ' -----')
@@ -5,8 +8,8 @@ def display_board(board):
     print('------ ' + '|' + board[6] + '|' + board[7] + '|' + board[8] + '|' + ' -----')
     print('------ ' + ' ----- ' + ' ----- ')
 
-test_board = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
-display_board(test_board)
+test_board = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+# display_board(test_board)
 
 def player_input():
     marker = ''
@@ -20,3 +23,47 @@ def player_input():
         return ('O', 'X')
 
 player_input()
+
+def place_marker(board, marker, position):
+    board[position] = marker
+    print(board[position])
+
+
+def win_check(board, mark):
+    return ((board[0] == mark and board[1] == mark and board[2] == mark) or
+    (board[3] == mark and board[4] == mark and board[5] == mark) or 
+    (board[6] == mark and board[7] == mark and board[8] == mark) or
+    (board[0] == mark and board[3] == mark and board[6] == mark) or
+    (board[1] == mark and board[4] == mark and board[7] == mark) or
+    (board[2] == mark and board[5] == mark and board[8] == mark) or
+    (board[0] == mark and board[4] == mark and board[8] == mark) or
+    (board[2] == mark and board[4] == mark and board[6] == mark))  
+
+def choose_first():
+    randomInt = random.randint(1, 10)
+    if randomInt <= 5:
+        return 'Player 1'
+    else:
+        return 'Player 2'
+
+def space_check(board, position):
+    return board[position] != 'X' or 'O'
+
+def full_board_check(board):
+    for space in range(1,10):
+        if space_check(board, space):
+            return False
+    return True
+
+def player_choice(board):
+    position = 0
+    while position not in [0,1,2,3,4,5,6,7,8] or not space_check(board, position):
+        position = int(input('Choose a position between 1-9')) - 1
+    return position
+
+def replay():
+    return input('Do you want to play again? (y or n)').lower.startswith('y')
+
+
+# place_marker(test_board, 'O', 0)
+# display_board(test_board)
